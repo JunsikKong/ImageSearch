aa628afb5df0c363c1b58cb8fd5d633cbb68cf48
js Kong <workjs1124@gmail.com>
2021-02-09 19:36:57 +0900

    다중검색기능 추가 코드가 난잡해서 날잡고 정리하기

--- ImageSearch/MainWindow.xaml.cs
+++ ImageSearch/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
@@ -81,14 +82,6 @@ namespace ImageSearch
 
         private string searchImg(Bitmap orgImg, Bitmap findImg)
         {
-            /* TemplateMatchModes.SqDiff = 0
-             * TemplateMatchModes.SqDiffNormed = 1
-             * TemplateMatchModes.CCorr = 2
-             * TemplateMatchModes.CCorrNormed = 3
-             * TemplateMatchModes.CCoeff = 4
-             * TemplateMatchModes.CCoeffNormed = 5
-             */
-
             string result = "";
 
             if (bmpOrigin != null && bmpFind != null)
@@ -119,8 +112,6 @@ namespace ImageSearch
                     {
                         Cv2.MatchTemplate(originMat, findMat, res, TemplateMatchModes.CCoeffNormed);
                     }
-
-                    
 
                     double minval, maxval = 0;
                     OpenCvSharp.Point minloc, maxloc;
@@ -158,8 +149,6 @@ namespace ImageSearch
                         result += "NOT FOUND";
                         imgOrigin.Source = bmp2BitmapImage(bmpOrigin);
                     }
-
-                    return result;
                 }
             }
             else if (bmpOrigin == null && bmpFind == null)
@@ -199,9 +188,108 @@ namespace ImageSearch
             tbxSpeed.Text = sw.ElapsedMilliseconds.ToString() + " ms";
         }
 
-        private void tbxOutput_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        private void btnMultiSearch_Click(object sender, RoutedEventArgs e)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
+            tbxOutput.Text = MultiSearchImg(bmpOrigin, bmpFind);
+
+            sw.Stop();
+
+            tbxSpeed.Text = sw.ElapsedMilliseconds.ToString() + " ms";
+        }
+
+        private string MultiSearchImg(Bitmap orgImg, Bitmap findImg)
+        {
+            string resultString = "";
+
+            if (bmpOrigin != null && bmpFind != null)
+            {
+                using (Mat srcMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(orgImg))
+                using (Mat dstMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(findImg))
+                {
+                    int result_cols = srcMat.Cols - dstMat.Cols + 1;
+                    int result_rows = srcMat.Rows - dstMat.Rows + 1;
+
+                    Mat res = new Mat(result_cols, result_rows, MatType.CV_32FC1);
+
+                    Cv2.MatchTemplate(srcMat, dstMat, res, TemplateMatchModes.CCoeffNormed);
+                    // Cv2.Normalize(res, res, 0, 1, NormTypes.MinMax, -1, new Mat());
+
+                    double minVal;
+                    double maxVal;
+                    OpenCvSharp.Point minLoc;
+                    OpenCvSharp.Point maxLoc;
+
+                    Cv2.Threshold(res, res, 0.9, 1, ThresholdTypes.Tozero);
+
+
+                    List<OpenCvSharp.Point> result = new List<OpenCvSharp.Point>();
+
+                    double peek_percent = (double)numHit.Value * 0.01;
+
+                    maxVal = 1.0;
+
+                    while (maxVal > peek_percent)
+                    {
+                        Cv2.MinMaxLoc(res, out minVal, out maxVal, out minLoc, out maxLoc);
+                        if (maxVal > peek_percent)
+                        {
+                            OpenCvSharp.Point pt1 = new OpenCvSharp.Point(maxLoc.X - dstMat.Cols / 2, maxLoc.Y - dstMat.Rows / 2);
+                            OpenCvSharp.Point pt2 = new OpenCvSharp.Point(maxLoc.X + dstMat.Cols / 2, maxLoc.Y + dstMat.Rows / 2);
+                            OpenCvSharp.Scalar sc = new OpenCvSharp.Scalar(0, 0, 0, 0);
+                            Cv2.Rectangle(res, pt1, pt2, sc, -1);
+                            result.Add(maxLoc);
+                        }
+                    }
+
+                    string str = "";
+
+                    for(int i=0; i<result.Count; i++)
+                    {
+                        str += result[i].X + ", " + result[i].Y + "\n";
+                    }
+
+                    imgOrigin.Source = MultiRectDraw(bmpOrigin, result);
+
+                    resultString = str;
+
+                }
+            }
+            else if (bmpOrigin == null && bmpFind == null)
+            {
+                resultString = "원본/찾는 이미지 상태 : NULL";
+            }
+            else if (bmpOrigin == null)
+            {
+                resultString = "원본 이미지 상태 : NULL";
+            }
+            else if (bmpFind == null)
+            {
+                resultString = "찾는 이미지 상태 : NULL";
+            }
+
+            return resultString;
+        }
+
+
+        private BitmapImage MultiRectDraw(Bitmap bmp, List<OpenCvSharp.Point> pt)
+        {
+            if (pt.Count > 0)
+            {
+                Bitmap bmpgr = new Bitmap(bmp);
+                Graphics bitmapGraphics = Graphics.FromImage(bmpgr);
+                System.Drawing.Pen pn = new System.Drawing.Pen(System.Drawing.Color.Red, 10);
+                for (int i=0; i<pt.Count; i++)
+                {
+                    bitmapGraphics.DrawRectangle(pn, pt[i].X, pt[i].Y, bmpFind.Width, bmpFind.Height);
+                }
+
+                return bmp2BitmapImage(bmpgr);
+            }
+            else return bmp2BitmapImage(bmp);
+            
         }
     }
 }
